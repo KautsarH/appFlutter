f3d5a8b3f4efcc1c3e7e536d8da9a050fb601513
KautsarH <kautsarhasanah@gmail.com>
2020-03-07 12:26:54 +0800

    Container

--- lib/main.dart
+++ lib/main.dart
@@ -74,49 +74,14 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Container(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Row(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Column(
-              children:<Widget>[
-                Icon(Icons.phone),
-                Container(
-                  child: Text('CALL')
-                )
-              ],
+          child: Center(
+            child: Container(
+              margin: const EdgeInsets.all(10.0),
+              color: Colors.amber[500],
+              width: 48.0,
+              height: 48.0,
             ),
-            Column(
-              children:<Widget>[
-                Icon(Icons.near_me),
-                Container(
-                    child: Text('ROUTE')
-                )
-              ],
-            ),
-            Column(
-              children:<Widget>[
-                Icon(Icons.share),
-                Container(
-                    child: Text('SHARE')
-                )
-              ],
-            )
-          ],
-        ),
+          ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
